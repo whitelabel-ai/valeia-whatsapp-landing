ae8700a3c78d29cc63e241937e398b024e275e50
propiter <pro.pedro0413@gmail.com>
2025-02-06 09:05:47 -0500

    delete trailingslash to nextconfig

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const nextConfig = {
   output: 'standalone',
   eslint: { ignoreDuringBuilds: true },
   images: { unoptimized: true },
-  trailingSlash: true,
+  trailingSlash: false,
 
   headers: async () => [
     {
